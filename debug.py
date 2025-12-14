953bf2215792acdeb8bb95e4a97768634f1c8b4f
mytischi <mat-naumov@yandex.ru>
2025-12-14 12:03:27 +0400

    oh ok there was too much changes brotha, but i glad it works

---
+++ debug.py
@@ -0,0 +1,14 @@
+#from main import get_book_text
+#from stats import get_num_words, get_num_characters 
+
+#print(get_num_characters())
+
+#def sort_on(dic):
+    #return dic[]
+#print(sort_on(get_num_characters()))
+
+#for i in get_num_characters():
+    #if i.isalpha():
+        #print(f"{i}: {get_num_characters()[i]}")
+
+
